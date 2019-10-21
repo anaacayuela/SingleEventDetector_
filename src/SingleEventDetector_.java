7d7b73d75c272bff7be92ae9c6ac72362917b3de
anaacayuela <54528366+anaacayuela@users.noreply.github.com>
2019-10-21 16:59:49 +0200

    Update SingleEventDetector_.java

--- src/SingleEventDetector_.java
+++ src/SingleEventDetector_.java
@@ -1,46 +1,28 @@
+import java.awt.Color;
+import java.awt.Frame;
+import java.util.*;
+import java.awt.image.*;
+
 import ij.*;
 import ij.process.*;
 import ij.text.TextWindow;
 import ij.gui.*;
 import ij.plugin.frame.*;
-import java.awt.Color;
-import java.awt.Dialog;
-import java.awt.Frame;
-import java.awt.Window;
-import java.util.*;
-import java.awt.Image;
-import ij.io.OpenDialog;
-import ij.macro.Interpreter;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
-import java.lang.Object;
-import ij.gui.*;
-import ij.process.*;
-import ij.text.*;
 import ij.io.*;
 import ij.plugin.*;
-import ij.plugin.filter.*;
-import ij.util.Tools;
-import ij.macro.Interpreter;
-import ij.macro.MacroRunner;
-import java.awt.*;
-import java.awt.image.*;
-import ij.*;
-import ij.gui.*;
 import ij.measure.*;
 
 public class SingleEventDetector_  implements PlugIn, Measurements  {
 
 	private static final float yif = 0;
+	private static final float xif = 0;
 	private int particleDiameter = 3;
-	private float pixelSize = 1;
+	private int pixelSize = 1;
 	private int slice=1;
 	private int frame=1;
 	private int[] particleMask;
 	private int boxN = 3;
-	private int boxSize = 2*boxN*particleDiameter+1;
+	private int boxSize;
 	ImagePlus impbFiltered;
 	ImagePlus impOriginal;
 	ImagePlus impbSelected;
@@ -49,6 +31,8 @@ public class SingleEventDetector_  implements PlugIn, Measurements  {
 	ImagePlus impb;
 	ImagePlus impbShow2;
 	ImagePlus impbShow3;
+	ImagePlus impbShow4;
+	ImagePlus impbShow5;
 	private int channel=1;
 	protected boolean invert; 
 	private static boolean doScaling = true;
@@ -62,18 +46,18 @@ public class SingleEventDetector_  implements PlugIn, Measurements  {
 	private float meanInside;
 	private float meanOutside;
 	private float std;
-	private float parameter=3;
+	private float top=10;
 	private float pooledVar;
 	private int points;
-	private String options;
+	private boolean bPass;
 	
 	public void drawParticleMask() {
-	
+		int R2=particleDiameter*particleDiameter/4;
 		particleMask = new int[boxSize*boxSize];
-		int center=boxN*particleDiameter;
+		int center=boxSize/2;
 		for (int i=0; i<boxSize; i++)
 			for (int j=0; j<boxSize; j++)
-				particleMask[i*boxSize+j]=((i-center)*(i-center)+(j-center)*(j-center)<=particleDiameter*particleDiameter)? 1:0;
+				particleMask[i*boxSize+j]=((i-center)*(i-center)+(j-center)*(j-center)<=R2)? 1:0;
 	}
 	
 	public ImageProcessor cropParticle(ImagePlus impbSelected, float xif, float yif)
@@ -81,14 +65,12 @@ public class SingleEventDetector_  implements PlugIn, Measurements  {
  		int [] dim=impbSelected.getDimensions();
 		int xi=Math.round(xif);
 		int yi=Math.round(yif);
-		int x0=xi-boxN*particleDiameter;//inicial
-		int xF=xi+boxN*particleDiameter;//final
-		int y0=yi-boxN*particleDiameter;
-		int yF=yi+boxN*particleDiameter;
+		int x0=xi-boxSize/2;
+		int xF=xi+boxSize/2;
+		int y0=yi-boxSize/2;
+		int yF=yi+boxSize/2;
 		if (x0>=0 && y0>=0 && xF<dim[0] && yF<dim[1])
-			
 		{
-			
 			impbSelected.setRoi(x0,y0,boxSize,boxSize);
 			return impbSelected.crop().getProcessor();
 		}
@@ -96,12 +78,12 @@ public class SingleEventDetector_  implements PlugIn, Measurements  {
 			return null;
 	}
 	
-	public boolean evaluateParticle(float xif, float yif)
+	public double evaluateParticle(float xif, float yif)
 	{
 		ImageProcessor piece=cropParticle(impbFiltered,xif,yif);
-		if (piece==null)
-			return false;
-		
+		if (piece== null)
+			return -1000;
+			
 		float sumOutside=0.0f, sumInside=0.0f, sum2Outside=0.0f, sum2Inside=0.0f, Noutside=0.0f, Ninside=0.0f;
 		for (int n=0; n<particleMask.length; n++)
 		{
@@ -126,10 +108,7 @@ public class SingleEventDetector_  implements PlugIn, Measurements  {
 		pooledVar=((Ninside-1)*varInside+(Noutside-1)*varOutside)/(Ninside+Noutside);
 		std=(float)Math.sqrt(pooledVar);
 		
-		parameter = (meanInside-meanOutside)/std;
-		return parameter>3;
-		
-		
+		return (meanInside-meanOutside)/std;
 	}
 
 	public void addImage(ImageProcessor ipb1, ImageProcessor ipb2)
@@ -155,11 +134,6 @@ public class SingleEventDetector_  implements PlugIn, Measurements  {
 	    }  		
 	}
 	
-	public void drawOval(int x, int y, int width, int height) {
-		if ((int)particleDiameter*particleDiameter>4*this.particleDiameter*this.particleDiameter) return;
-		
-	}
-
 	@SuppressWarnings("deprecation")
 	public void run(String arg) {
 		ImagePlus imp = IJ.getImage();
@@ -182,87 +156,93 @@ public class SingleEventDetector_  implements PlugIn, Measurements  {
  		}
  	}
  		if (impInicial.getNChannels() > 1) { 
-		channelToExtract=channel;
- 		impb = extractChannel(impInicial, channelToExtract);
+		   channelToExtract=channel;
+		   impb = extractChannel(impInicial,channelToExtract);
  		}
  		if (impInicial.getNSlices() > 1) {
- 		sliceToExtract=slice;
- 		impb=extractZSlice(impInicial, sliceToExtract);
- 		impbShow = impb.duplicate();
+ 		   sliceToExtract=slice;
+ 		   impb=extractZSlice(impInicial, sliceToExtract);
+ 		   impbShow = impb.duplicate();
  		}
- 		if (impInicial.getNFrames() > 1) {
- 		 frameToExtract=frame;
- 		 impb = extractTFrame(impInicial, frameToExtract);
- 		 impbShow3 = impb.duplicate();
+ 		if (impInicial.getNChannels() == 1 &&impInicial.getNFrames() > 1) {
+ 		   frameToExtract=frame;
+ 		   impb = extractTFrame(impInicial, frameToExtract);
+ 		   impbShow3 = impb.duplicate();
  		}
-		if(impInicial.getNChannels() > 1 && impInicial.getNSlices() > 1 && impInicial.getStackSize() > 1) {
-		 sliceToExtract=slice;
-		 channelToExtract=channel;
-		 impb =extractSliceChannel(impInicial, sliceToExtract, channelToExtract);
+		if (impInicial.getNChannels() > 1 && impInicial.getNSlices() > 1 && impInicial.getStackSize() > 1) {
+		   sliceToExtract=slice;
+		   channelToExtract=channel;
+		   impb =extractSliceChannel(impInicial, sliceToExtract, channelToExtract);
+		   impbShow5= impb.duplicate();
 		}
-		if(impInicial.getNChannels() > 1 && impInicial.getNSlices() > 1  && impInicial.getNFrames() >  1 && impInicial.getStackSize() > 1){
-			 sliceToExtract=slice;
-			 channelToExtract=channel;
-			 frameToExtract=frame;
-			 impb =extractSliceChannelFrame(impInicial, sliceToExtract, channelToExtract,frameToExtract);
-			 impbShow2 = impb.duplicate();
+		if (impInicial.getNChannels() > 1 && impInicial.getNFrames() > 1 && impInicial.getStackSize() > 1) {
+			   frameToExtract=frame;
+			   channelToExtract=channel;
+			   impb =extractFrameChannel(impInicial, sliceToExtract, channelToExtract);
+			   impbShow4=impb.duplicate();
+			}
+		if (impInicial.getNChannels() > 1 && impInicial.getNSlices() > 1  && impInicial.getNFrames() >  1 && impInicial.getStackSize() > 1){
+			sliceToExtract=slice;
+			channelToExtract=channel;
+			frameToExtract=frame;
+			impb =extractSliceChannelFrame(impInicial, sliceToExtract, channelToExtract,frameToExtract);
+			impbShow2 = impb.duplicate();
 		}
-		
- 		IJ.run(impb, "Bandpass Filter...", String.format(Locale.ROOT,
- 				"filter_large=%d filter_small=%d suppress=None tolerance=5 autoscale saturate",
- 				3*particleDiameter, particleDiameter));
- 		
- 		impbFiltered=impb.duplicate();
+		double stdParticle=particleDiameter/6.0;
+		boxSize = (int)Math.ceil(boxN*particleDiameter/2+1);
+		impbFiltered = impb.duplicate();
  		impbSelected=impb.duplicate();
-		
+ 		if (bPass == true ) {
+ 		IJ.run(impb, "Bandpass Filter...",  String.format(Locale.ROOT,"filter_large=%d filter_small=%d suppress=None tolerance=5 autoscale saturate",particleDiameter,particleDiameter/3));
+ 		}
  		IJ.run(impb, "Detect Molecules", String.format(Locale.ROOT,
- 				"task=[Detect molecules and fit] psf=%d pixel=1 parallel=500 fitting=10 mark ignore",
- 				particleDiameter));
+ 				"task=[Detect molecules and fit] psf=%f pixel=%d parallel=500 fitting=10 mark ignore",
+ 				stdParticle,pixelSize));
  		impb.hide();
- 		Frame FR = WindowManager.getFrame("Results");
- 		if (FR instanceof TextWindow) 
- 			((TextWindow)FR).hide();
+ 		
  		
  		ResultsTable RT = ResultsTable.getResultsTable(); 		
  		
- 		float [] x = RT.getColumn(0);//vector float con valores x de la tabla de resultados
- 		float [] y = RT.getColumn(1);//vector float con los valores y de la tabla de resultados
- 		boolean [] coordMask = new boolean[x.length];
+ 		float [] x = RT.getColumn(0);
+ 		float [] y = RT.getColumn(1);
+ 		if (x==null)
+ 			return;
+ 	
+ 		double [] coordZscore = new double[x.length];
  		drawParticleMask();
  		int Nok=0;
  		FloatProcessor ipPSF = new FloatProcessor(boxSize, boxSize);
  		for (int i=0; i<x.length; i++)
- 		{
-         	coordMask[i]=evaluateParticle(x[i],y[i]);
- 			
- 			if (coordMask[i]==true) {
- 				OvalRoi oval = new OvalRoi((int)(x[i]-0.5*boxSize), 
-                        (int)(y[i]-0.5*boxSize),
-                       boxSize, boxSize);	
+         	coordZscore[i]=evaluateParticle(x[i],y[i]);
+		
+ 		double [] coordZscoreSorted = new double[x.length];
+ 		System.arraycopy(coordZscore, 0, coordZscoreSorted, 0, coordZscore.length);
+ 		Arrays.sort(coordZscoreSorted);
+ 		double threshold=coordZscoreSorted[(int)((1-Math.max(Math.min(top,100),0)/100.0)*x.length)];
+ 		
+   		for (int i=0; i<x.length; i++)
+     	{
+ 			 if(coordZscore[i]>threshold)
+ 			 {
+ 				OvalRoi oval = new OvalRoi((int)(x[i]-0.5*particleDiameter), (int)(y[i]-0.5*particleDiameter),
+ 						particleDiameter, particleDiameter);	
  				
  				impbSelected.getProcessor().draw(oval);
- 				int width = boxSize;
+ 				int width =boxSize;
  				impbSelected.getProcessor().setLineWidth(width);
  				impbSelected.getProcessor().setColor(Color.RED);
   				addImage(ipPSF, cropParticle(impbSelected,x[i],y[i]));
  	 			Nok+=1;
- 			
- 	 		 oval2 = new OvalRoi((int)(x[i]-0.5*boxSize), 
-                        (int)(y[i]-0.5*boxSize),
-                       boxSize, boxSize);
- 	 		
- 	 		RoiManager manager = RoiManager.getInstance();
- 	 		  if (manager == null)
- 	 		     {
+ 	 			
+
+ 	 		    RoiManager manager = RoiManager.getInstance();
+ 	 		    if (manager == null)
  	 		        manager = new RoiManager();
- 	 		     }
- 	 		  manager.addRoi(oval2);
- 	 		  
+                manager.addRoi(oval);
 			}
- 			
  		}
- 		if (imp.getNChannels() > 1) {
- 	 		RoiManager rmOne = RoiManager.getInstance();
+   		if (imp.getNChannels() > 1) {
+   			RoiManager rmOne = RoiManager.getInstance();
  	 		imp = IJ.getImage();
  	 		IJ.run("From ROI Manager", "");
  	 		ImagePlus imp2 = HyperStackConverter.toHyperStack(imp, imp.getNChannels(), imp.getNSlices(),imp.getNFrames(),"Color");
@@ -291,7 +271,7 @@ public class SingleEventDetector_  implements PlugIn, Measurements  {
   			rmThree.runCommand(impbShow,"Show All with labels");
   			imp.close();
   		 }
- 		if (imp.getNFrames() > 1) {
+ 		if (impInicial.getNChannels() == 1 && imp.getNFrames() > 1) {
  			RoiManager rmSix = RoiManager.getInstance();
  	 		impbShow3.show();
  	 		IJ.run("From ROI Manager", "");
@@ -322,6 +302,19 @@ public class SingleEventDetector_  implements PlugIn, Measurements  {
  	 		impInicial.hide();
  	 		rmFive.runCommand(impbShow2,"Show All with labels");
  	 		imp.close();
+ 		}
+ 		if(imp.getNChannels() > 1 && imp.getNFrames() > 1 && imp.getStackSize() > 1) {
+ 			RoiManager rmSix = RoiManager.getInstance();
+ 	 		imp = IJ.getImage();
+ 	 		IJ.run("From ROI Manager", "");
+ 	 		ImagePlus imp2 = HyperStackConverter.toHyperStack(imp, imp.getNChannels(), imp.getNSlices(),imp.getNFrames(),"Color");
+ 	 		impOriginal.hide();
+ 	 		impInicial.hide();
+ 	 		imp2.show();
+ 	 		rmSix.runCommand(imp2,"Show All with labels");
+ 	 		imp.close();
+ 	 		imp.close();
+ 	 		
  		}
  		ic3 = new ImageConverter(impbSelected);
  		ic3.convertToGray8();
@@ -363,20 +356,16 @@ public class SingleEventDetector_  implements PlugIn, Measurements  {
       	Frame FR2 = WindowManager.getFrame(cf.getFormula());
  		if (FR2 instanceof TextWindow) 
  			((TextWindow)FR2).setTitle("PSF Profile");
+ 		Frame FR = WindowManager.getFrame("Results");
+ 		if (FR instanceof TextWindow) 
+ 			((TextWindow)FR).close(false);
  		
 }
 	boolean showDialog(ImagePlus imp) {
 		GenericDialog gd = new GenericDialog("Single Event Detector");
-		if (imp.getStackSize() == 1) {
-			gd.addNumericField("Particle diameter", particleDiameter, 0, 3, "pixels");
-			gd.addNumericField("Pixel size", 1.0, 0, 3, "nm");
-			gd.addNumericField("(meanInside-meanOutside)/std",parameter,0,3, null);
-		}
-		if (imp.getStackSize() > 1) {
-			gd.addNumericField("Particle diameter", particleDiameter, 0, 3, "pixels");
-			gd.addNumericField("Pixel size", 1.0, 0, 3, "nm");
-			gd.addNumericField("(meanInside-meanOutside)/std",parameter,0,3, null);
-		}
+		gd.addNumericField("Pixel size", pixelSize, 0, 3, "nm");
+		gd.addNumericField("Particle diameter", particleDiameter, 0, 3, "pixels");
+		gd.addNumericField("Percentage of points to keep",top,0,3, null);
 		if (imp.getNChannels() > 1) {
     		gd.addStringField("Channel to process ( 1,2,3,...)", "1");
 		}
@@ -386,7 +375,7 @@ public class SingleEventDetector_  implements PlugIn, Measurements  {
 		if (imp.getNFrames() > 1) {
 			gd.addNumericField("Frame to process",frame,0,3, "frame");
 		}
-		
+		gd.addCheckbox("Apply BandPass Filter (IF THE IMAGE IS TOO NOISY!!!!!)", false);
 		gd.showDialog();
 		if(gd.wasCanceled())
 			return false;
@@ -394,13 +383,9 @@ public class SingleEventDetector_  implements PlugIn, Measurements  {
 			IJ.error("Error", "Invalid input number");
 			return false;
 		}				
+		pixelSize = (int)(gd.getNextNumber());
 		particleDiameter = (int)(gd.getNextNumber());
-		pixelSize = (float)(gd.getNextNumber());
-		parameter = (float)(gd.getNextNumber());
-		if(imp.getNFrames() > 1)
-		frame =(int)(gd.getNextNumber());
-		if (impInicial.getNSlices() > 1)
-		slice=(int)(gd.getNextNumber());	
+		top = (float)(gd.getNextNumber());
 		if (imp.getNChannels() > 1) {
 			String channelStr= gd.getNextString();
 			if (channelStr  =="R")
@@ -412,6 +397,11 @@ public class SingleEventDetector_  implements PlugIn, Measurements  {
 			else
 				channel=Integer.parseInt(channelStr);
 		}
+		if (impInicial.getNSlices() > 1)
+			slice=(int)(gd.getNextNumber());	
+		if(imp.getNFrames() > 1)
+			frame =(int)(gd.getNextNumber());
+		bPass = (boolean) gd.getNextBoolean();
 		return true;
 	}
 	
@@ -431,8 +421,7 @@ public class SingleEventDetector_  implements PlugIn, Measurements  {
                 stack2.addSlice("", imp.getStack().getProcessor(sliceOne));
             }
     	
-        ColorModel cm = LookUpTable.createGrayscaleColorModel(false);
-        stack2.setColorModel(cm);
+     
         imp2.setStack(stack2);
         imp2.setDimensions(1, zslices, frames);
         if (zslices * frames > 1)
@@ -440,12 +429,6 @@ public class SingleEventDetector_  implements PlugIn, Measurements  {
         imp2.setFileInfo(fileInfo);
         imp2.updateAndDraw();
         return imp2;
-    }
-	private ImagePlus extractSlice(ImagePlus imp, int slice) {
-		imp.setSlice(slice);
-		ImageProcessor ip = imp.getProcessor();
-		ImagePlus imp2 = new ImagePlus("Slice " + slice + "-" + imp.getTitle(), ip);
-		return imp2;
 	}
 	private ImagePlus extractZSlice(ImagePlus imp, int slice) {
 		  int width    = imp.getWidth();
@@ -506,8 +489,7 @@ public class SingleEventDetector_  implements PlugIn, Measurements  {
 		      int sliceThree = imp.getStackIndex(channel, slice, f);
 		      stack2.addSlice("", imp.getStack().getProcessor(sliceThree));
 		  }
-	      ColorModel cm = LookUpTable.createGrayscaleColorModel(false);
-	      stack2.setColorModel(cm);
+	     
 		  imp2.setStack(stack2);
 		  imp2.setDimensions(1, 1, frames);
 		  if (frames > 1)
@@ -528,11 +510,33 @@ public class SingleEventDetector_  implements PlugIn, Measurements  {
 		  ImagePlus imp2 = new ImagePlus("C" + channel + "-" +"Z" + slice + "-"+"T" + frame + "-"+ imp.getTitle(), bp);
 		  int sliceFour = imp.getStackIndex(channel, slice, frame);
 	      stack2.addSlice("", imp.getStack().getProcessor(sliceFour));
-	      ColorModel cm = LookUpTable.createGrayscaleColorModel(false);
-	      stack2.setColorModel(cm);
+	     
 		  imp2.setStack(stack2);
 		  imp2.setDimensions(1, 1, 1);
 		  imp2.setOpenAsHyperStack(true);
+		  imp2.setFileInfo(fileInfo);
+		  return imp2;
+	}
+	private ImagePlus extractFrameChannel(ImagePlus imp, int frame, int channel) {
+		  int width    = imp.getWidth();
+		  int height   = imp.getHeight();
+		  int channels = imp.getNChannels();
+		  int frames   = imp.getNFrames();
+		  int zslices = imp.getNSlices();
+		  FileInfo fileInfo = imp.getOriginalFileInfo();
+		  ImageStack stack2 = new ImageStack(width, height);
+		  ColorProcessor cp = new ColorProcessor(imp.getImage());
+	     	  ByteProcessor  bp = cp.getChannel(channel, null);
+		  ImagePlus imp2 = new ImagePlus("C" + channel + "-" +"T" + frame + "-"+ imp.getTitle(), bp);
+		  for (int z = 1; z <= zslices; z++) {
+		      int sliceThree = imp.getStackIndex(channel, z, frame);
+		      stack2.addSlice("", imp.getStack().getProcessor(sliceThree));
+		  }
+	     
+		  imp2.setStack(stack2);
+		  imp2.setDimensions(1, zslices, 1);
+		  if (zslices > 1)
+		    imp2.setOpenAsHyperStack(true);
 		  imp2.setFileInfo(fileInfo);
 		  return imp2;
 	}
